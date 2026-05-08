b467be4af75f39b80d50e0b6a6c4aa8e3bb05ed0
Xavi Aznar <60074936+xaznar@users.noreply.github.com>
2026-05-08 14:19:55 +0200

    Delete flan-pwa directory

--- flan-pwa/sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE = 'flan-v1';
-const ASSETS = ['./index.html', './manifest.json'];
-self.addEventListener('install',  e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',    e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
